8ade1415c203032b8f0e9284264cb01260520591
Fluke <Guppiya2010@hotmail.com>
2025-09-20 14:27:13 +0700

    new UI login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,34 +1,59 @@
 <template>
   <v-container fill-height>
-    <v-row class="justify-center align-center pl-5">
+    <v-row class="justify-center align-center">
       <v-card style="width:400px;">
         <!-- <v-img
           class="white--text align-end"
           src="@/assets/login_bg.jpg"
           height="200px"
         > -->
-        <v-card-title primary-title style="color:#254e58">
-          Login
+         <!-- <v-row class="justify-center align-center">
+         <v-col cols="12" md="12" class="justify-center align-center"> -->
+             <v-card-title class="justify-center align-center">
+        <v-img
+            src="/MVibes_Logo.jpg"
+            color="#ffffff"
+            alt=""
+            height="150px"
+            width="150px"
+            contain
+          />
+          </v-card-title>
+           <!-- </v-col>
+          </v-row> -->
+        <v-card-title primary-title style="color:#1F51FF;font-size:22px;" class="justify-center align-center mt-n8">
+          M/C VIBES TRANSMISSION
+        </v-card-title>
+          <v-card-title primary-title style="color:#000000;font-size: 14px;" class="justify-center align-center mt-n8">
+         Smart Monitoring for Machine Vibrations
         </v-card-title>
         <!-- </v-img> -->
         <v-card-text>
           <v-form @submit.prevent="submit">
-            <!-- Username -->
-            <v-text-field name="username" label="Username" id="username" v-model="account.username" outlined dense />
+           <h3 class="black--text pb-1">Username</h3> 
+            <v-text-field name="username" label="Enter your username" id="username" v-model="account.username" outlined dense />
 
-            <!-- Password -->
-            <v-text-field class="heightsize40" name="password" label="Password" id="password" v-model="account.password"
+               <h3 class="black--text pb-1">Password</h3> 
+            <v-text-field class="heightsize40" name="password" label="Enter your password" id="password" v-model="account.password"
               :append-icon="isShowPassword ? 'visibility' : 'visibility_off'"
               @click:append="isShowPassword = !isShowPassword" :type="isShowPassword ? 'text' : 'password'" outlined
               dense />
-
-            <v-row>
-              <v-btn style="color:#254e58;margin-top:18px;" text @click.prevent="forgotPassword()">Forgot Password
-                ?</v-btn>
+              
+            <v-row class="px-3 pt-10">
+              <v-btn block dark type="submit" color="#1F51FF"> Login </v-btn>
             </v-row>
 
-            <v-row class="px-3 pt-3 pb-3">
-              <v-btn block dark type="submit" color="#1F51FF"> Login </v-btn>
+             <v-row class="mt-n2">
+              <!-- #254e58 -->
+               <v-col cols="12" md="6" class="justify-center align-center">
+              <v-btn style="color:#1F51FF;margin-top:18px;font-size: 12px;" text @click.prevent="forgotPassword()">Forgot Password
+                ?</v-btn>
+                </v-col>
+                 <v-col cols="12" md="6" class="justify-center align-center">
+                  <!-- @click.prevent="forgotPassword()" -->
+                     <v-btn style="color:#1F51FF;margin-top:18px;font-size: 12px;" text >Contact Support
+                ?</v-btn>
+                 </v-col>
             </v-row>
           </v-form>
         </v-card-text>
